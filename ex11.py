ec69c8184d362b98daff5e58076f37b35573ffd2
austinmscholl <asprof34@gmail.com>
2018-10-19 00:17:59 -0400

    ex11.py

---
+++ ex11.py
@@ -0,0 +1,15 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input("Really?")
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+print("What city were you born in?", end=' ')
+city = input()
+print("What is your age?", end= ' ')
+realAge = input('Really?')
+
+print(f"So you're actually {realAge} years old, and from {city}.")
