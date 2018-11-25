34b9f5e004f2775c88fce922bdea8c1a3414cbf0
austinmscholl <asprof34@gmail.com>
2018-11-25 12:55:31 -0500

    new excercises

--- ex13.py
+++ ex13.py
@@ -1,8 +1,14 @@
 from sys import argv
 # read the WYSS section how to run this
-script, first, second, third = argv
+script, age, second, third = argv
+
+# asks the user for input value
+age = input("How old are you? ")
+
+# prints the string with age variable
+# print(f"So, you're {age} old.")
 
 print("The script is called:", script)
-print("Your first variable is:", first)
+print("Your first variable is:", age)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
